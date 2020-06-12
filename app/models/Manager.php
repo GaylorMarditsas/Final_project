bc56dbb4f28ba8fb542d2d500b76c4b16efb68fe
Gaylor Marditsas <gaylor273@outlook.fr>
2020-06-12 16:01:30 +0200

    test pgsql

--- app/models/Manager.php
+++ app/models/Manager.php
@@ -8,7 +8,8 @@ class Manager
     protected function dbConnect()
     {
         try {
-            $bdd= new \PDO('mysql:host=localhost;dbname=projet;charset=utf8', 'root', '');
+            //$bdd= new \PDO('mysql:host=localhost;dbname=projet;charset=utf8', 'root', '');
+            $bdd= new \PDO('pgsql:host=ec2-54-247-78-30.eu-west-1.compute.amazonaws.com;port=5432;dbname=d7ka6qi6do46kk', 'xiggkiboyskswu', '5835f48e95a00a252064803a9cdbc69565419869ce790f66192a689e517ec2ef');
             return $bdd;
         } catch (Exeption $e) {
             die('Erreur : ' . $e->getMessage());
