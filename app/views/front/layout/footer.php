5fc51d0f29dfcec524aed7c3134995360a7174cb
Gaylor Marditsas <gaylor273@outlook.fr>
2020-06-14 21:02:57 +0200

    cgu

--- app/views/front/layout/footer.php
+++ app/views/front/layout/footer.php
@@ -1,5 +1,8 @@
 <footer>
-<a href="index.php?action=mentions">Mentions Légales</a>
-<a href="index.php?action=cgu">CGU</a>
-<a href="">Plan du site</a>
+    <ul>
+        <li><a href="index.php?action=mentions">Mentions Légales</a></li>
+        <li><a href="index.php?action=cgu">CGU</a></li>
+        <li><a href="">Plan du site</a></li>
+    </ul>
+
 </footer>
