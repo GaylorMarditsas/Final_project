7f1ef2c36c5006b9bd124f5a5dcc766b800c189e
Gaylor Marditsas <gaylor273@outlook.fr>
2020-06-15 21:35:54 +0200

    barre de cookie

--- app/views/front/contact.php
+++ app/views/front/contact.php
@@ -23,6 +23,5 @@
   src="https://code.jquery.com/jquery-3.5.0.min.js"
   integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ="
   crossorigin="anonymous"></script>
-<script src="app/public/js/creds.js"></script>
 <script src="app/public/js/index.js"></script>
 </body>
