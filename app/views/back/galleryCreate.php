f2e24d0839a17ce466255bb347bb5fa315cd97db
Gaylor Marditsas <gaylor273@outlook.fr>
2020-06-18 10:46:15 +0200

    debug

--- app/views/back/galleryCreate.php
+++ app/views/back/galleryCreate.php
@@ -6,7 +6,7 @@ $gods = $createImage->viewBack();
 <!-- GESTION DE L'AJOUT D'IMAGE DANS LA GALERIE -->
     <h1 class="admin-title lato">Ajouter des images</h1>
     <section class="godback">
-        <form class="backform lato" enctype="multipart/form-data" action="" method="post" name="create-gallery">
+        <form class="backform lato" enctype="multipart/form-data" action="indexBack.php?action=createImage" method="post" name="create-gallery">
             <div>
             <select  name="name">
 
